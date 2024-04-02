f3df157c6ace37011f1979eae7ca05339e4d226a
NKuzmin <nkuzmin@korusconsulting.ru>
2024-04-02 22:50:41 +0300

    Отображение статуса синхронизации на клиентской части

--- Schemas/MainHeaderSchema/MainHeaderSchema.js
+++ Schemas/MainHeaderSchema/MainHeaderSchema.js
@@ -1,29 +1,55 @@
  define("MainHeaderSchema", ["css!KnMainHeaderSchemaCss"], function(){
 	return {
 		attributes: {
-			"LastGitSyncSessinLabel": {
+			"LastGitSyncSessionLabel": {
 				dataValueType: this.BPMSoft.DataValueType.DateTime,
 			},
+			"GitSyncSessionStatusLabel": {
+				dataValueType: this.BPMSoft.DataValueType.DateTime,
+			},
+			"IsErrorLogoVisible": {
+				dataValueType: this.BPMSoft.DataValueType.Boolean,
+				value: false
+			}
 		},
-		messages: { },
 		methods: {
+			/**
+			 * Инициализация модуля
+			 */
 			init: function() {
 				this.callParent(arguments);
-				
-				BPMSoft.SysSettings.querySysSettingsItem("KnLastBpmToGitSyncSessionDate", function(value) {
-					if (value) {
-						var diffInHours = ((new Date() - value) / 3600000).toFixed(1);
-						this.set(
-							"LastGitSyncSessinLabel", 
-							Ext.String.format(this.get("Resources.Strings.LastGitSyncSessionTemplateLabel"), diffInHours)
-						);
-					}
+				this.updateGitSyncLabel();	
+			},
+
+			/**
+			 * Инициализирует состояние индикатора
+			 * синхронизации с git-репозиторием
+			 */
+			updateGitSyncLabel: function() {
+				BPMSoft.SysSettings.querySysSettings(
+					["KnBpmToGitSyncStatus", "KnLastBpmToGitSyncSessionDate", "KnBpmToGitSyncMessage"], 
+					function(values) {
+
+						if (values.KnLastBpmToGitSyncSessionDate) {
+							var diffInHours = ((new Date() - values.KnLastBpmToGitSyncSessionDate) / 3600000).toFixed(1);
+							this.set(
+								"LastGitSyncSessionLabel", 
+								Ext.String.format(this.get("Resources.Strings.LastGitSyncSessionTemplateLabel"), diffInHours)
+							);
+						
+							if (values.KnBpmToGitSyncStatus) {
+								this.set(
+									"GitSyncSessionStatusLabel", 
+									Ext.String.format(this.get("Resources.Strings.GitSyncSessionStatusTemplateLabel"), values.KnBpmToGitSyncMessage , diffInHours)
+								);
+
+								this.set("IsErrorLogoVisible", values.KnBpmToGitSyncStatus.toLowerCase() === "error");
+							}
+						}	
 				}, this);
-				
 			}
 		},
 		diff: [
-			//main header menu logo
 			{
 				"operation": "insert",
 				"name": "LastGitSyncSessionContainer",
@@ -38,15 +64,28 @@
 			},
 			{
 				"operation": "insert",
+				"name": "LastGitSyncSessionStatusContainer",
+				"parentName": "LastGitSyncSessionContainer",
+				"propertyName": "items",
+				"values": {
+					"itemType": BPMSoft.ViewItemType.LABEL,
+					"labelClass": ["git-sync-label git-sync-error-label"],
+					"caption": {"bindTo": "GitSyncSessionStatusLabel"},
+					"visible": {"bindTo": "IsErrorLogoVisible"}
+				}
+			},
+			{
+				"operation": "insert",
 				"name": "LastGitSyncSessionDateTimeContainer",
 				"parentName": "LastGitSyncSessionContainer",
 				"propertyName": "items",
 				"values": {
 					"itemType": BPMSoft.ViewItemType.LABEL,
-					"labelClass": ["last-git-sync-session-label"],
-					"caption": {"bindTo": "LastGitSyncSessinLabel"},
+					"labelClass": ["git-sync-label last-git-sync-session-date-label"],
+					"caption": {"bindTo": "LastGitSyncSessionLabel"},
+					"visible": {"bindTo": "IsErrorLogoVisible", "bindConfig": {"converter": "invertBooleanValue"}}
 				}
-			},
+			}
 		]
 	};
 });
